e8211e22eca5bcaf20d39d6ee02f159173f5b3d8
RodrigoViana29 <88325159+RodrigoViana29@users.noreply.github.com>
2023-10-06 11:01:54 -0300

    Update CadastroClientes.java

--- CadastroClientes.java
+++ CadastroClientes.java
@@ -2,33 +2,63 @@ import java.util.Scanner;
 
 public class CadastroClientes {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Informe quantos clientes deseja cadastrar no máximo 50: ");
-		int qtd = 0;
-		
-		qtd = sc.nextInt();
-		while (qtd > 50) {
-			System.out.println("Digite um valor menor que 50: ");
-			qtd = sc.nextInt();
-		}
-		Cliente  clientes[] = new Cliente[qtd];
-		for (int i = 0; i<clientes.length;i++) {
-			clientes[i] = new Cliente();
-			System.out.println("Informe o código do cliente: ");
-			clientes[i].setCodigo(sc.nextInt());
-			System.out.println("Informe o nome do cliente: ");
-			clientes[i].setNome(sc.next());
-			System.out.println("Informe o cpf do cliente: ");
-			clientes[i].setCpf(sc.next());
-			System.out.println("Informe o telefone do cliente: ");
-			clientes[i].setTelefone(sc.next());
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+Cliente clientes[] = new Cliente[50];
+for (int i = 0;i< 50;i++) {
+	clientes[i] = new Cliente();
+}
+int qtd = 0;
+int menu = 0;
+System.out.println("digite 1 para cadastrar cliente");
+System.out.println("digite 2 para buscar cliente pelo nome");
+System.out.println("digite 3 para exibir dados dos clientes");
+System.out.println("digite 4 para sair");
+menu = sc.nextInt();
+
+	while (menu != 4) {
+			
+			if (menu == 1) {
+				if (clientes.length < 51) {
+					Cliente novocliente = new Cliente();
+					System.out.println("Informe o código do cliente: ");
+					novocliente.setCodigo(sc.nextInt());
+					System.out.println("Informe o nome do cliente: ");
+					novocliente.setNome(sc.next());
+					System.out.println("Informe o cpf do cliente: ");
+					novocliente.setCpf(sc.next());
+					System.out.println("Informe o telefone do cliente: ");
+					novocliente.setTelefone(sc.next());
+					clientes[qtd] = novocliente;
+					qtd++;
+				}
+				else {
+					System.out.println("limite esgotado");
+				}
 				
-		}
-		System.out.println("O número de clientes é: " + clientes.length);
-		for(int i = 0; i< clientes.length;i++) {
-			System.out.println(clientes[i]);
+				
+				}
+				
+					
+				
+			if (menu == 2) {
+				String nome = sc.nextLine();
+				for(int i = 0; i<qtd;i++) {
+					//if (nome == )
+					
+					System.out.println(clientes[i]);
+				}
+			}
+			if (menu == 3) {
+				for(int i = 0; i<qtd;i++) {
+					System.out.println(clientes[i]);
+				}
+			}
+			System.out.println("digite 1,2,3 ou 4 denovo");
+			menu = sc.nextInt();
+			
 		}
 	sc.close();	
 	}
+			
 }
