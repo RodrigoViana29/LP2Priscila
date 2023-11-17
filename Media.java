5ee061f0e7ae4c41da7e50ad4c5d62837bad8a0f
RodrigoViana29 <88325159+RodrigoViana29@users.noreply.github.com>
2023-11-17 10:31:05 -0300

    Media.java

--- Media.java
+++ Media.java
@@ -1,22 +1,31 @@
 import javax.swing.JOptionPane;
-
 public class Media {
-
 	public static void main(String[] args) {
 		String n1,n2;
 		double media;
 		n1 = JOptionPane.showInputDialog(null, "Digite a primeira nota");
 		n2 = JOptionPane.showInputDialog(null, "Digite a segunda nota");
-		media = (Double.parseDouble(n1)+ Double.parseDouble(n2))/2;
-		if (media > 7) {
-			JOptionPane.showInternalMessageDialog(null, "O aluno está aprovado com media: "+ media, "Media do Aluno", -1);
+		if (!n1.isEmpty()) {
+			if (!n2.isEmpty()) {
+				media = (Double.parseDouble(n1)+ Double.parseDouble(n2))/2;
+				if (media >= 7) {
+					JOptionPane.showInternalMessageDialog(null, "O aluno está aprovado com media: "+ media, "Media do Aluno", -1);
+			
+				}
+				else {
+					JOptionPane.showInternalMessageDialog(null, "O aluno está reprovado e com média: "+ media, "Média do Aluno", -1);
+				}
+			}
+			else {
+				JOptionPane.showInternalMessageDialog(null, "Nota 2 não digitada", "Erro",JOptionPane.ERROR_MESSAGE );
+			}
 			
 		}
 		else {
-			JOptionPane.showInternalMessageDialog(null, "O aluno está reprovado e com média: "+ media, "Média do Aluno", -1);
+			JOptionPane.showInternalMessageDialog(null, "Nota 1 não digitada", "Erro",JOptionPane.ERROR_MESSAGE );
 		}
+		
 		
 
 	}
-
 }
